394ef1c30171d315b3e919632471a72b10a700e7
VeselovskiyAleksandr <79366865+VeselovskiyAleksandr@users.noreply.github.com>
2022-04-13 15:06:39 +0300

    test 1.

--- ConsoleApplication1.29.1/ConsoleApplication1.29.1.cpp
+++ ConsoleApplication1.29.1/ConsoleApplication1.29.1.cpp
@@ -1,5 +1,5 @@
 ﻿// ConsoleApplication1.29.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+//Урок 26. Задача 1. Реализация имитации аудиоплеера.
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <locale.h>
@@ -12,19 +12,15 @@ using namespace std;
 
 class Player;
 
-
-
 class Track {
-public:
-	string title, dateCreation, duration;
 	friend int record(Track melody[1000]);
-
+public:
 		static int load() {
 			ifstream file("C:\\Users\\Александр\\Documents\\text for program\\playerM.txt");
 			string str = "", str1 = "";
 			int j = 0, count = 0;		
 			if (file.is_open()) {
-				cout << "\nThe file is open.\n";
+				cout << "\nЗагрузка мелодий.\n";
 			}
 			else {
 				cerr << "\nThe file is not found. ";
@@ -33,8 +29,7 @@ public:
 			while (!file.eof()) {				
 				file >> melody[j].title >> melody[j].duration;
 				str1 = "";
-			//	for (int l = 0; l < 6; l++) {
-				while(str1!=":"){
+				for (int l = 0; l < 6; l++) {
 					str1 = "";
 					file >> str1;
 					str.append(str1);
@@ -47,17 +42,18 @@ public:
 				j++; 
 			}
 	file.close();	
-	//return 0;
 		}
+string title, dateCreation, duration;
 }
 melody[1000];
 
+class Player {
 static int TrackSize() {
 		int count = 0;
-		ifstream file("C:\\Users\\Александр\\Documents\\text for program\\playerM.txt");
-		string str = "";
+		ifstream file("C:\\Users\\Александр\\Documents\\text for program\\playerM.txt"); 
+	    string str = "";
 		if (file.is_open()) {
-			cout << "\nThe file is open.";
+			cout << "\nСейчас звучит:";
 		}
 		else {
 			cerr << "\nThe file is not found. ";
@@ -74,12 +70,9 @@ static int TrackSize() {
 		file.close();
 	return count;
 	}
-
-class Player {
 public:
 	static int  record(Track melody[1000]) {
-ofstream file("C:\\Users\\Александр\\Documents\\text for program\\playerM.txt", ios::app);
-			
+ofstream file("C:\\Users\\Александр\\Documents\\text for program\\playerM.txt", ios::app);	
 int count = 0;
 			string str = "";
 			if (file.is_open()) {
@@ -108,83 +101,91 @@ file << strTitle << " " << strDur << " " << loc << ":" << "\n";
 			if (strR == "end") {
 				break;
 			}
-			
 			strTitle = ""; strDur = "";
-		}
-
-		
+		}		
 return 0;
 };
+
 	static void exit() {
 	cout << "\nВыключение плеера.";
 }
 
-static bool pause (string str) {
-	if (str == "pause") return true;	
-	else return false;
+static void pause (int count) {
+	if (count == 0) {
+		cout << "\nПауза.";
+	}
 }
 
-	static void play() {
-	//	bool st=Player:: pause (str);
-	//	while (str != true) {
-			int count = 0, numMelody = 0, interim = 0;
-			Track::load();
-			count = TrackSize();
-			srand(time(nullptr));
-			numMelody = rand() % count + 1;
-			interim = stoi(melody[numMelody - 1].duration);
-			time_t  start = time(nullptr);
-			time_t completion = start + (time_t)interim;
-			cout << "\n" << melody[numMelody - 1].title << " " << melody[numMelody - 1].duration << " " << melody[numMelody - 1].dateCreation;
-			while (completion > start) {
-				start = time(nullptr);
+	static void play(int count, string strP, int counter) {
+		if (strP != "pause") {
+			if (count == 0) {
+				int numMelody = 0;
+				cout << "\n" << melody[counter - 1].title << " " << melody[counter - 1].duration << " " << melody[counter - 1].dateCreation;
+			}
+			else if (count != 0) {
+				cout << "\n" << melody[counter - 1].title << " " << melody[counter - 1].duration << " " << melody[counter - 1].dateCreation;
 			}
 		}
-   // }
+	}
+
+   static int next() {
+		int countMel = 0, numM = 0;
+		countMel = TrackSize();
+		srand(time(nullptr));
+		numM = rand() % countMel + 1;
+		return numM;
+    }
+
+   static void stop(int count) {
+		if (count == 0) {
+			cout << "\nВоспроизведение остановлено.";
+		}
+    }
 };
-
-
-
-
-
-void next() {
-
-}
-
-void stop() {
-
-}
-
-
- 
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-	string str4 = "", str5="", strAct="";
-	int counter=0;
+	string str4 = "", str5="", strAct="", strP="";
+	int countPause=0, countMel=0, numMelody = 0, countS=0;
 	cout << "\n                               АУДИОПЛЕЕР.";
-	cout << "\nНастройки аудиоплеера: play - воспроизведение звукозаписи;";
+	cout << "\nНастройки аудиоплеера: ";
+	cout << "\n                       on   - включение аудиоплеера;";
+	cout << "\n                       play - воспроизведение звукозаписи;";
 	cout << "\n                       pause - пауза;";
 	cout << "\n                       next - переход к следующей записи;";
 	cout << "\n                       stop - прекращение воспроизведения;";
 	cout << "\n                       record - запись;";
 	cout << "\n                       exit - выключение плеера.";
 	while (strAct != "exit") {
-		cout << "\nВведите действие.";
 		cin >> strAct;
+		if (strAct == "on") {
+			Track::load();
+		}
 		if (strAct == "record") {
 			Player::record(melody);
 		}
-		else if (strAct == "play") {
-			Player::play();
+		else if (strAct == "play"|| strAct == "next") {
+			if (countPause == 0) {
+				numMelody = Player::next();
+			}
+			Player::play(countPause, strAct, numMelody);
+			countS = 0;
 		}
-		else if (strAct == "load") {
-			Track::load();
+		else if (strAct == "pause") {
+			Player::pause(countPause);
+			countPause++;		
+		}
+		else if (strAct == "stop") {
+			Player::stop (countS);
+			countS++;
 		}
 		else if (strAct == "exit") {
 			Player::exit();
 			break;
+		}
+       if (strAct != "pause" && countPause > 0) {
+			countPause = 0;
 		}
 	}
 	return 0;
